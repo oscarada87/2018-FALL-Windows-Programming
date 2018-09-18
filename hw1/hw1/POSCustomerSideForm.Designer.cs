14139f3578b9dfa54d67b9227464a3aea16cf790
Oscar <oscarada87@gmail.com>
2018-09-18 23:32:59 +0800

    finish ui

---
+++ hw1/hw1/POSCustomerSideForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace hw1
+{
+    partial class POSCustomerSideForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.MealsGroupBox = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.PageLabel = new System.Windows.Forms.Label();
+            this.PreviousPageButton = new System.Windows.Forms.Button();
+            this.NextPageButton = new System.Windows.Forms.Button();
+            this.recordDataGridView = new System.Windows.Forms.DataGridView();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.MealsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.recordDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.AddButton.Location = new System.Drawing.Point(362, 571);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(141, 31);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // MealsGroupBox
+            // 
+            this.MealsGroupBox.Controls.Add(this.button12);
+            this.MealsGroupBox.Controls.Add(this.button11);
+            this.MealsGroupBox.Controls.Add(this.button10);
+            this.MealsGroupBox.Controls.Add(this.button9);
+            this.MealsGroupBox.Controls.Add(this.button8);
+            this.MealsGroupBox.Controls.Add(this.button7);
+            this.MealsGroupBox.Controls.Add(this.button6);
+            this.MealsGroupBox.Controls.Add(this.button5);
+            this.MealsGroupBox.Controls.Add(this.button3);
+            this.MealsGroupBox.Controls.Add(this.PageLabel);
+            this.MealsGroupBox.Controls.Add(this.PreviousPageButton);
+            this.MealsGroupBox.Controls.Add(this.NextPageButton);
+            this.MealsGroupBox.Controls.Add(this.AddButton);
+            this.MealsGroupBox.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MealsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.MealsGroupBox.Name = "MealsGroupBox";
+            this.MealsGroupBox.Size = new System.Drawing.Size(536, 651);
+            this.MealsGroupBox.TabIndex = 7;
+            this.MealsGroupBox.TabStop = false;
+            this.MealsGroupBox.Text = "Meals";
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button12.Location = new System.Drawing.Point(362, 367);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(150, 150);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "麥脆雞 （2塊）\r\n$ 104元";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button11.Location = new System.Drawing.Point(190, 367);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(150, 150);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "勁辣雞腿堡\r\n$ 69元";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button10.Location = new System.Drawing.Point(19, 367);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(150, 150);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "麥克雞塊（9塊）\r\n$ 89元";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button9.Location = new System.Drawing.Point(362, 205);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "麥克雞塊 （6塊）\r\n$ 59元";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button8.Location = new System.Drawing.Point(190, 205);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "麥香雞\r\n$ 49元";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button7.Location = new System.Drawing.Point(19, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "嫩煎雞腿堡\r\n$ 79元";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button6.Location = new System.Drawing.Point(362, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "安格斯黑牛堡\r\n$ 99元";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button5.Location = new System.Drawing.Point(190, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "雙層牛肉吉事堡\r\n$ 59元";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button3.Location = new System.Drawing.Point(19, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "大麥克\r\n$ 69元";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // PageLabel
+            // 
+            this.PageLabel.AutoSize = true;
+            this.PageLabel.ForeColor = System.Drawing.Color.SteelBlue;
+            this.PageLabel.Location = new System.Drawing.Point(20, 610);
+            this.PageLabel.Name = "PageLabel";
+            this.PageLabel.Size = new System.Drawing.Size(93, 24);
+            this.PageLabel.TabIndex = 6;
+            this.PageLabel.Text = "Page: 1/2";
+            // 
+            // PreviousPageButton
+            // 
+            this.PreviousPageButton.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.PreviousPageButton.Location = new System.Drawing.Point(215, 608);
+            this.PreviousPageButton.Name = "PreviousPageButton";
+            this.PreviousPageButton.Size = new System.Drawing.Size(141, 31);
+            this.PreviousPageButton.TabIndex = 5;
+            this.PreviousPageButton.Text = "Previos Page";
+            this.PreviousPageButton.UseVisualStyleBackColor = true;
+            // 
+            // NextPageButton
+            // 
+            this.NextPageButton.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.NextPageButton.Location = new System.Drawing.Point(362, 608);
+            this.NextPageButton.Name = "NextPageButton";
+            this.NextPageButton.Size = new System.Drawing.Size(141, 31);
+            this.NextPageButton.TabIndex = 4;
+            this.NextPageButton.Text = "Next Page";
+            this.NextPageButton.UseVisualStyleBackColor = true;
+            // 
+            // recordDataGridView
+            // 
+            this.recordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.recordDataGridView.Location = new System.Drawing.Point(592, 24);
+            this.recordDataGridView.Name = "recordDataGridView";
+            this.recordDataGridView.RowTemplate.Height = 24;
+            this.recordDataGridView.Size = new System.Drawing.Size(606, 572);
+            this.recordDataGridView.TabIndex = 8;
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.TotalLabel.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.TotalLabel.Location = new System.Drawing.Point(1014, 613);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(184, 38);
+            this.TotalLabel.TabIndex = 7;
+            this.TotalLabel.Text = "Total: 200元";
+            // 
+            // POSCustomerSideForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 675);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.recordDataGridView);
+            this.Controls.Add(this.MealsGroupBox);
+            this.Name = "POSCustomerSideForm";
+            this.Text = "Form1";
+            this.MealsGroupBox.ResumeLayout(false);
+            this.MealsGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.recordDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.GroupBox MealsGroupBox;
+        private System.Windows.Forms.Button PreviousPageButton;
+        private System.Windows.Forms.Button NextPageButton;
+        private System.Windows.Forms.Label PageLabel;
+        private System.Windows.Forms.DataGridView recordDataGridView;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+
+    }
+
+}
+
