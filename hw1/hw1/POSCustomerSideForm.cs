5d068d70e7c99a402ba4f787945ec32e7c6bf22b
Oscar <oscarada87@gmail.com>
2018-09-22 21:05:17 +0800

    finish hw1

--- hw1/hw1/POSCustomerSideForm.cs
+++ hw1/hw1/POSCustomerSideForm.cs
@@ -12,6 +12,9 @@ namespace POSCustomerSide
 {
     public partial class POSCustomerSideForm : Form
     {
+        const int ZERO = 0;
+        const int NINE = 9;
+        const int FIFTEEN = 15; 
         const int PRICE49 = 49;
         const int PRICE59 = 59;
         const int PRICE69 = 69;
@@ -118,8 +121,8 @@ namespace POSCustomerSide
             _model.AddMeal("BLT辣脆雞腿堡", PRICE109);
         }
 
-        //按下第一頁新增按鈕
-        private void AddButton1Click(object sender, EventArgs e)
+        //按下新增按鈕
+        private void AddButtonClick(object sender, EventArgs e)
         {
             List<Meal> mealList = _model.GetMealList();
 
@@ -137,35 +140,27 @@ namespace POSCustomerSide
         }
 
         //按下上一頁
-        private void PreviousPageButton2Click(object sender, EventArgs e)
+        private void ClickPreviousPage(object sender, EventArgs e)
         {
-            _mealsGroupBox1.Visible = true;
-            _mealsGroupBox2.Visible = false;
+            for (int i = ZERO; i < NINE; i++)
+                _mealsGroupBox.Controls[i].Visible = true;
+            for (int i = NINE; i < FIFTEEN; i++)
+                _mealsGroupBox.Controls[i].Visible = false;
+            _nextPageButton.Enabled = true;
+            _previousPageButton.Enabled = false;
+            _pageLabel1.Text = "Page: 1/2";
         }
 
         //按下下一頁
-        private void NextPageButton1Click(object sender, EventArgs e)
+        private void ClickNextPage(object sender, EventArgs e)
         {
-            _mealsGroupBox1.Visible = false;
-            _mealsGroupBox2.Visible = true;
-        }
-
-        //按下第二頁新增按鈕
-        private void AddButton2Click(object sender, EventArgs e)
-        {
-            List<Meal> mealList = _model.GetMealList();
-
-            mealList.ForEach(x =>
-            {
-                DataGridViewRow row = new DataGridViewRow();
-                row.CreateCells(_mealGridView);
-                row.Cells[0].Value = x.Name;
-                row.Cells[1].Value = x.Price;
-                _mealGridView.Rows.Add(row);
-            });
-            _model.AddToDisplayMealList();
-            _model.ClearMealList();
-            _totalLabel.Text = "Total: " + _model.GetTotalPrice() + "元";
+            for (int i = ZERO; i < NINE; i++)
+                _mealsGroupBox.Controls[i].Visible = false;
+            for (int i = NINE; i < FIFTEEN; i++)
+                _mealsGroupBox.Controls[i].Visible = true;
+            _nextPageButton.Enabled = false;
+            _previousPageButton.Enabled = true;
+            _pageLabel1.Text = "Page: 2/2";
         }
     }
 }
